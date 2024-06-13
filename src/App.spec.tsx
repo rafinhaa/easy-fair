77e21a98a7baade1f552108b2dafa48cbd094756
Rafinhaa <rafinhaa@me.com>
2024-06-13 15:29:38 -0300

    chore: jest

---
+++ src/App.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react-native"
+import App from "./App"
+
+describe("App", () => {
+  it("should render", () => {
+    render(<App />)
+
+    expect(
+      screen.getByText("Open up App.js to start working on your app!"),
+    ).toBeTruthy()
+  })
+})
