b792842e8043bbe89edfc7075449098fd0a8f31f
Rafinhaa <rafinhaa@me.com>
2024-06-17 09:47:40 -0300

    feat: unused imports and vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,23 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "unused-imports"],
   rules: {
     "prettier/prettier": [
       "error",
       {
         endOfLine: "auto",
+      },
+    ],
+    "no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
       },
     ],
   },
